9d3c30db9d0cb7f837a03e8483cfed8b4ee19c9d
John Doolittle <jdoo.code@gmail.com>
2020-10-08 10:16:54 -0600

    Applied font styles, installed additional dependencies

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div id='productInfo'></div>
+    <div id='relatedProducts'></div>
+    <div id='qa'></div>
+    <div id='reviews'></div>
+  )
+}
